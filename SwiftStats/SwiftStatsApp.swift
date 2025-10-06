acd410a9291635858c36416c40cda9436af23682
Rob J Goedman <goedman@icloud.com>
2025-10-06 14:41:02 -0600

    Initial Commit

---
+++ SwiftStats/SwiftStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStatsApp.swift
+//  SwiftStats
+//
+//  Created by Robert Goedman on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftStatsTests/SwiftStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStatsTests.swift
+//  SwiftStatsTests
+//
+//  Created by Robert Goedman on 10/6/25.
+//
+
+import Testing
+@testable import SwiftStats
+
+struct SwiftStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
