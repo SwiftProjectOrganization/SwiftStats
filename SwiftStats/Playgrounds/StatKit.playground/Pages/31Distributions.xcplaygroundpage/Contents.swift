b2c4f79b0e39c911987d7af71de3f51afd134fc4
Rob J Goedman <goedman@icloud.com>
2025-10-11 10:18:36 -0600

    Added live views to Distributions playground with Swift Charts

--- SwiftStats/Playgrounds/StatKit.playground/Pages/31Distributions.xcplaygroundpage/Contents.swift
+++ SwiftStats/Playgrounds/StatKit.playground/Pages/31Distributions.xcplaygroundpage/Contents.swift
@@ -3,6 +3,65 @@
 //: ## 31 Distributions
 import Foundation
 import StatKit
+import SwiftUI
+import PlaygroundSupport
+import Charts
+
+struct SalesData: Identifiable {
+    let id = UUID()
+    let month: String
+    let sales: Int
+}
+
+let salesData = [
+    SalesData(month: "Jan", sales: 200),
+    SalesData(month: "Feb", sales: 150),
+    SalesData(month: "Mar", sales: 180),
+    SalesData(month: "Apr", sales: 900),
+    SalesData(month: "May", sales: 1050),
+    SalesData(month: "Jun", sales: 3600),
+    SalesData(month: "Jul", sales: 4900),
+    SalesData(month: "Aug", sales: 5120),
+    SalesData(month: "Sep", sales: 3500),
+    SalesData(month: "Oct", sales: 2700),
+    SalesData(month: "Nov", sales: 1050),
+    SalesData(month: "Dec", sales: 80),
+]
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, charts world!")
+            Chart {
+                ForEach(salesData) { data in
+                    BarMark(
+                        x: .value("Month", data.month),
+                        y: .value("Sales", data.sales)
+                    )
+                }
+            }
+            .frame(width: 400, height: 200)
+            .padding()
+            Chart {
+                ForEach(salesData) { data in
+                    LineMark(
+                        x: .value("Month", data.month),
+                        y: .value("Sales", data.sales)
+                    )
+                }
+            }
+            .frame(height: 200)
+            .padding()
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
+
+
 
 let normal = NormalDistribution(mean: 0, variance: 1)
 normal.cdf(x: 0)
